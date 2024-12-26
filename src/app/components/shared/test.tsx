37b5029e7824abc0c4bc5ad81325b54c50e8c204
zakariae37 <zakariaelakhdar1937@gmail.com>
2024-12-26 21:12:16 +0100

    style: format code according to project guidelines

--- src/app/components/shared/test.tsx
+++ src/app/components/shared/test.tsx
@@ -9,7 +9,15 @@ const projects = [
     image: "/placeholder.svg?height=200&width=300",
     link: "#"
   },
- 
+
+
+  {
+    title: "Project 1",
+    description: "A brief description of Project 1",
+    image: "/placeholder.svg?height=200&width=300",
+    link: "#"
+  },
+  
  
 ]
 
