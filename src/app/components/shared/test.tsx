ddd17af303e139bd15a1101bb52e90698da2196a
zakariae37 <zakariaelakhdar1937@gmail.com>
2025-01-03 22:37:41 +0100

    second test

--- src/app/components/shared/test.tsx
+++ src/app/components/shared/test.tsx
@@ -101,12 +101,7 @@ const projects = [
     image: "/placeholder.svg?height=200&width=300",
     link: "#"
   },
-  {
-    title: "Project 1",
-    description: "A brief description of Project 1",
-    image: "/placeholder.svg?height=200&width=300",
-    link: "#"
-  },
+  
   
  
 ]
