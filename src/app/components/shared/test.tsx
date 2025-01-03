2a18ab700fe49a614bb0017e92b6d21e5d4fb4a3
zakariae37 <zakariaelakhdar1937@gmail.com>
2025-01-03 22:38:25 +0100

    rebuild main ui

--- src/app/components/shared/test.tsx
+++ src/app/components/shared/test.tsx
@@ -88,17 +88,7 @@ const projects = [
     link: "#"
   },
 
-  {
-    title: "Project 1",
-    description: "A brief description of Project 1",
-    image: "/placeholder.svg?height=200&width=300",
-    link: "#"
-  },
-
- 
   
-  
- 
 ]
 
 export default function Projects() {
