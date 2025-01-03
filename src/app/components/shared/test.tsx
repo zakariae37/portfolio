cfb8f1ea29309f439c52107c18c648e4301b1a4b
zakariae37 <zakariaelakhdar1937@gmail.com>
2025-01-03 22:38:03 +0100

    second fix

--- src/app/components/shared/test.tsx
+++ src/app/components/shared/test.tsx
@@ -95,12 +95,7 @@ const projects = [
     link: "#"
   },
 
-  {
-    title: "Project 1",
-    description: "A brief description of Project 1",
-    image: "/placeholder.svg?height=200&width=300",
-    link: "#"
-  },
+ 
   
   
  
